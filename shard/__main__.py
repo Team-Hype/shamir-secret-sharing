c1ade1a6ca0ec4c85d1cd4201f489a2d58d17139
Dmitrii Prosvirkin <prosvirkin.dmitry@gmail.com>
2025-04-21 20:09:49 +0300

    feat: gitignore + main arg parser

--- shard/__main__.py
+++ shard/__main__.py
@@ -1,2 +1,32 @@
+import argparse
+import sys
+
+def start_master():
+    print("Starting in MASTER mode...")
+    # TODO
+
+def start_slave(master_host):
+    print(f"Starting in SLAVE mode... Connecting to Master at {master_host}")
+    # TODO
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="SHamir\nAlgorithm\nReliability\nDistributed")
+
+    parser.add_argument('--mode', choices=['master', 'slave'], required=True, help="Run mode: master or slave")
+    parser.add_argument('--master-host', help="Master host address (required in slave mode)")
+
+    args = parser.parse_args()
+
+    if args.mode == 'master':
+        if args.master_host:
+            print("Error: --master-host should not be used in master mode.")
+            sys.exit(1)
+        start_master()
+    elif args.mode == 'slave':
+        if not args.master_host:
+            print("Error: --master-host is required in slave mode.")
+            sys.exit(1)
+        start_slave(args.master_host)
+
     print("-- Shard --")
+
