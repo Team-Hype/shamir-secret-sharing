685283e4a76a6592360b42a8a1508469ec67c83d
Dmitrii Prosvirkin <prosvirkin.dmitry@gmail.com>
2025-04-22 13:39:35 +0300

    Add gprc services sceleton + db init

--- shard/__main__.py
+++ shard/__main__.py
@@ -4,18 +4,21 @@ import sys
 import shard.slave as slave
 import shard.master as master
 
-def start_master():
+def start_master(port: str):
     print("Starting in MASTER mode...")
-    master.start()
+    master.start(port)
 
-def start_slave(master_host):
-    print(f"Starting in SLAVE mode... Connecting to Master at {master_host}")
-    slave.start()
+def start_slave(master_host, port):
+    print("Starting in SLAVE mode...")
+    slave.start(master_host, port=port)
 
 if __name__ == '__main__':
+    print("-- Shard --")
+
     parser = argparse.ArgumentParser(description="SHamir Algorithm Reliable Distributed")
 
     parser.add_argument('--mode', choices=['master', 'slave'], required=True, help="Run mode: master or slave")
+    parser.add_argument('--port', required=True, help="Grpc Server Port")
     parser.add_argument('--master-host', help="Master host address (required in slave mode)")
 
     args = parser.parse_args()
@@ -24,11 +27,10 @@ if __name__ == '__main__':
         if args.master_host:
             print("Error: --master-host should not be used in master mode.")
             sys.exit(1)
-        start_master()
+        start_master(args.port)
     elif args.mode == 'slave':
         if not args.master_host:
             print("Error: --master-host is required in slave mode.")
             sys.exit(1)
-        start_slave(args.master_host)
+        start_slave(args.master_host, args.port)
 
-    print("-- Shard --")
