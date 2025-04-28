566ff9fccc7cb9ff05c309689f39a3d0a6d2ed72
peplxx <91543105+peplxx@users.noreply.github.com>
2025-04-28 23:47:11 +0300

    feat: tests

--- package/noxfile.py
+++ package/noxfile.py
@@ -14,5 +14,6 @@ def lint(session):
 
 @nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12", "3.13"])
 def test(session):
+    session.install(".")
     session.install("pytest")
     session.run("pytest")
